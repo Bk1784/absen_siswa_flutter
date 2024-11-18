9a021e8af00ae4713dd813f27ca426f269876446
bk1784 <futonabuu456@gmail.com>
2024-11-18 11:01:39 +0700

    perbaikan koce 1

--- lib/screens/home_screens.dart
+++ lib/screens/home_screens.dart
@@ -1,8 +1,8 @@
 import 'package:absen_mahasiswa2/providers/attendance_providers.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'attendance_history_screen.dart';  // Halaman Riwayat Kehadiran
-      // Provider untuk manajemen kehadiran
+import 'attendance_history_screen.dart'; 
+     
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -12,12 +12,12 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  int _currentIndex = 0; // Menyimpan index tab yang aktif
+  int _currentIndex = 0; 
 
   final List<Widget> _screens = [
-    // Daftar halaman yang bisa dipilih pada BottomNavigationBar
-    const AttendancePage(),  // Halaman Pencatatan Kehadiran
-    const AttendanceHistoryScreen(),  // Halaman Riwayat Kehadiran
+   
+    const AttendancePage(),  
+    const AttendanceHistoryScreen(), 
   ];
 
   @override
@@ -25,14 +25,14 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.blueAccent,
-        title: const Text('Pencatatan Kehadiran'),
+        title: const Text(style: TextStyle(color: Colors.white),'Pencatatan Kehadiran'),
       ),
-      body: _screens[_currentIndex],  // Menampilkan halaman yang dipilih
+      body: _screens[_currentIndex], 
       bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _currentIndex,  // Menandakan halaman aktif
+        currentIndex: _currentIndex, 
         onTap: (index) {
           setState(() {
-            _currentIndex = index;  // Mengubah halaman saat tab dipilih
+            _currentIndex = index;
           });
         },
         items: const [
@@ -75,7 +75,7 @@ class AttendancePage extends StatelessWidget {
                 );
               },
               separatorBuilder: (context, index) {
-                return const Divider();  // Menambahkan garis pemisah
+                return const Divider();  
               },
             ),
           ),
